1593cbe9a7ed75e61c4419a3b9f327a8c94619a6
davi-OD <davidodie51@gmail.com>
2023-12-06 18:25:02 +0300

    Landing page 90% done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 
+import AboutSection from '@/components/AboutSection'
+import ContactSection from '@/components/ContactSection'
+import FeaturesSection from '@/components/FeaturesSection'
+import FooterSection from '@/components/FooterSection'
 import Header from '@/components/Header'
 import HeroSection from '@/components/HeroSection'
+import PropertyListing from '@/components/PropertyListing'
+import TestimonialSection from '@/components/TestimonialSection'
 
 
 export default function Home() {
   return (
-   <div>
-    <Header />
-    <HeroSection />
-   </div>
+    <div>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <div id='listing'>
+        <PropertyListing />
+        <ContactSection />
+        <TestimonialSection />
+      </div>
+      <FooterSection />
+    </div>
   )
 }
