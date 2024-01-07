ebfb725d287166981cde32b9367a34aa557b7ff8
Bertin <bertin.m2001@gmail.com>
2024-01-07 19:07:01 +0200

    land recordong

---
+++ src/app/land-records/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import DashboardLayout from '../../components/dashboard/sharedContentWrapper/ContentLayout';
+
+
+function page() {
+  return (
+    <DashboardLayout>
+      <div className='flex-grow bg-white text-black p-8'>
+        <div className='w-full text-center'>
+          <h1 className='text-4xl font-bold'>Land reords</h1>
+          <p className='mt-4 text-lg'>land recording content goes here...</p>
+        </div>
+      </div>
+    </DashboardLayout>
+    
+  )
+}
+
+export default page
